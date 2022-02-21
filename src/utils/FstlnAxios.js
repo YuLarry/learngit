e95b5e4e466d3029a4df30d9056879ef37c94423
lijunwei-fastlane <junwei@fstln.io>
2022-02-21 11:11:52 +0800

    Updated axios config baseurl

--- src/utils/FstlnAxios.js
+++ src/utils/FstlnAxios.js
@@ -1,7 +1,7 @@
 /*
  * @Author: lijunwei
  * @Date: 2021-11-24 11:36:44
- * @LastEditTime: 2022-02-17 15:16:27
+ * @LastEditTime: 2022-02-21 11:10:29
  * @LastEditors: lijunwei
  * @Description: 
  */
@@ -12,8 +12,7 @@ import axios from "axios";
 const { REACT_APP_API_ORIGIN, REACT_APP_API_VERSION } = process.env;
 
 const ax = axios.create({
-  // baseURL: `${REACT_APP_API_ORIGIN}/api/v${REACT_APP_API_VERSION}/`,
-  baseURL: `${REACT_APP_API_ORIGIN}`,
+  baseURL: `${REACT_APP_API_ORIGIN}/api/pms`,
   timeout: 20000,
 })
 
